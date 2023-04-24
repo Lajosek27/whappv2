4240842ebc00aa844b6723b91ced279ff79b36e1
Maciej Ciepliński <106655898+Lajosek27@users.noreply.github.com>
2023-04-24 18:51:30 +0200

    Make migration

--- migrations/Version20230424165032.php
+++ migrations/Version20230424165032.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230423135936 extends AbstractMigration
+final class Version20230424165032 extends AbstractMigration
 {
     public function getDescription(): string
     {
