54c7d3228edefbbbf94df06600e51e302481ca05
Maciej Ciepliński <106655898+Lajosek27@users.noreply.github.com>
2023-04-25 20:01:32 +0200

    add displaying character

--- public/js/characterSheet.js
+++ public/js/characterSheet.js
@@ -9,3 +9,59 @@ function updateValue(input){
     console.log(hidden)
     console.log(span)
 }
+
+function selectProfession(id){
+
+    profession = document.querySelector('div[data-prof-id="'+ id +'"]')
+    professions = document.querySelectorAll('#professionList .profession')
+    input = document.getElementById('form_charcterId')
+    button = document.querySelector('footer .rightButtons a')
+    
+    input.value = input.value === profession.dataset.profId ? 0 : profession.dataset.profId
+    
+    input.value === 0 ? button.classList.add('disabled') : button.classList.remove('disabled')
+    console.log(profession)
+
+    professions.forEach(prof => {
+        (prof.querySelector('.comet-wrapper').classList.contains('show') && prof != profession) ? comet(prof) : '';
+
+    });
+   comet(profession)
+    // const myTimeout = setTimeout(() => {
+    //     cometLeft.classList.toggle('show')
+    //     cometRight.classList.toggle('show')
+    // }, 3333);
+    
+    
+
+}
+
+function comet(profession){
+
+  let cometLeft= profession.querySelector('.comet-wrapper.left')
+  let cometRight= profession.querySelector('.comet-wrapper.right')
+
+    if(cometLeft.classList.contains('show')){
+        cometLeft.classList.toggle('hidding')
+        cometRight.classList.toggle('hidding')
+        cometLeft.classList.toggle('show')
+        cometRight.classList.toggle('show')
+        setTimeout(() => {
+                cometLeft.classList.toggle('hide')
+                cometRight.classList.toggle('hide')
+                cometLeft.classList.toggle('hidding')
+                cometRight.classList.toggle('hidding')
+             }, 0333);
+    }else{
+        cometLeft.classList.toggle('showing')
+        cometRight.classList.toggle('showing')
+        cometLeft.classList.toggle('hide')
+        cometRight.classList.toggle('hide')
+        setTimeout(() => {
+                cometLeft.classList.toggle('show')
+                cometRight.classList.toggle('show')
+                cometLeft.classList.toggle('showing')
+                cometRight.classList.toggle('showing')
+             }, 0333);
+    }
+}
--- src/Controller/CharacterSheetController.php
+++ src/Controller/CharacterSheetController.php
@@ -2,13 +2,17 @@
 
 namespace App\Controller;
 
+use App\Entity\Character;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 use App\Services\charactersService;
 use App\Entity\CharacterInfo;
 use App\Form\CharacterInfoType;
-
+use App\Repository\ProfessionRepository;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Symfony\Component\Form\FormFactoryInterface;
 
 class CharacterSheetController extends AbstractController
 {
@@ -32,8 +36,15 @@ class CharacterSheetController extends AbstractController
             return $this->redirectToRoute('app_catalog');
         }
 
-
         $character = $characterGetter->getCharacter($characterId);
+
+        if($action == 'edit' && $character->getPlayer() !== $this->getUser() && $character->getGameMaster() !== $this->getUser() )
+        {
+            $this->addFlash('error', 'Nie posiadasz dostępu do rządanego zasobu :/');
+            return $this->redirectToRoute('app_login');
+        }
+
+        
         
         $formInfo = $this->createForm(CharacterInfoType::class, $character->getInfo());
 
@@ -42,6 +53,69 @@ class CharacterSheetController extends AbstractController
             'character' => $character,
             'edit' => $action==='edit'? true : false,
             'formInfo' => $formInfo,
+
+        ]);
+    }
+
+    #[Route('/set/profession/{characterId}', name: 'app_set_profession',  requirements: ['characterId' => '\d+'])]
+    public function setProfession(charactersService $characterGetter,ProfessionRepository $profRepo,FormFactoryInterface $formFactory,Request $request,int $characterId = 0)
+    {
+        if(!$this->getUser())
+        {   
+            $this->addFlash('error', 'Nie posiadasz dostępu do rządanego zasobu :/');
+            return $this->redirectToRoute('app_login');
+        }
+        if($characterId <= 0)
+        {   
+            $this->addFlash('error', 'Nie ma takiej postaci :/');
+            return $this->redirectToRoute('app_catalog');
+        }
+        
+        $character = $characterGetter->getCharacter($characterId);
+        
+        if( $character->getPlayer() !== $this->getUser() && $character->getGameMaster() !== $this->getUser() )
+        {
+            $this->addFlash('error', 'Nie posiadasz dostępu do rządanego zasobu :/');
+            return $this->redirectToRoute('app_login');
+        }
+
+        $profs = $profRepo->findAll();
+
+        $form = $formFactory->createBuilder()
+        ->add('charcterId', HiddenType::class,[
+            'attr' => ['value' => 0]
+            ])
+        ->getForm();
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $input = $form->get('charcterId')->getData();
+            if (!filter_var($input, FILTER_VALIDATE_INT, ['options' => ['min_range' => 0]]) !== false) {
+                $this->addFlash('error', 'Coś poszło nie tak :/'. $input);
+                return $this->render('character_sheet/setProfession.html.twig', [
+                    'character' => $character,
+                    'professions' => $profs,
+                    'form' => $form->createView()
+        
+                ]);
+            }
+
+            
+            
+
+           
+
+            $this->addFlash('succes', 'Twoje dane to: '.$input );
+            return $this->redirectToRoute('app_character_sheet',[
+                "characterId" => $characterId,
+                'action' => 'edit'
+            ]);
+        }
+        
+        return $this->render('character_sheet/setProfession.html.twig', [
+            'character' => $character,
+            'professions' => $profs,
+            'form' => $form->createView()
 
         ]);
     }
